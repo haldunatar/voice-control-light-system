7dca9c3d0effa0f7ff54514d06244302e73121d0
Haldun Atar <atarhaldun@gmail.com>
2016-09-17 22:31:57 +0200

    gpio pins setup

--- server.js
+++ server.js
@@ -8,3 +8,21 @@ app.get('/', (req, res) =>  res.render('./index.html'));
 
 app.listen('3000', () => console.log('running on 3000'));
 
+
+//Gpio pins setup
+const lightBlue = new Gpio(1, 'out');
+const lightGreen = new Gpio(2, 'out');
+const lightRed = new Gpio(3, 'out');
+const lightYellow = new Gpio(4, 'out');
+
+const lightBlueOn = () => lightBlue.writeSync(1);
+const lightBlueOff = () => lightBlue.writeSync(0);
+
+const lightGreenOn = () => lightGreen.writeSync(1);
+const lightGreenOff = () => lightGreen.writeSync(0);
+
+const lightRedOn = () => lightRed.writeSync(1);
+const lightRedOff = () => lightRed.writeSync(0);
+
+const lightYellowOn = () => lightYellow.writeSync(1);
+const lightYellowOff = () => lightYellow.writeSync(0);
